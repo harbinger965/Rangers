564f284998c0dc675074b6cbc248f675d121a0dd
kkiniaes <kkiniaes@gmail.com>
2016-04-08 07:45:43 -0400

    derp

--- Assets/PauseUI.cs
+++ Assets/PauseUI.cs
@@ -10,7 +10,6 @@ public class PauseUI : MonoBehaviour {
 
 	public bool pauseMenuOpen;
 
-
 	public Selectable startingButton;
 
 	// Use this for initialization
@@ -27,7 +26,7 @@ public class PauseUI : MonoBehaviour {
 				pause2.rectTransform.anchoredPosition3D = Vector3.MoveTowards(pause2.rectTransform.anchoredPosition3D,new Vector3(1.5f,0,0),pause2.rectTransform.anchoredPosition3D.x*Time.deltaTime);
 				pause3.rectTransform.anchoredPosition3D = Vector3.MoveTowards(pause3.rectTransform.anchoredPosition3D,Vector3.zero,pause3.rectTransform.anchoredPosition3D.y*Time.deltaTime);
 			}
-			Navigator.defaultGameObject = 
+			Navigator.defaultGameObject = startingButton.gameObject;
 		} else {
 			transform.GetChild(0).GetComponent<RectTransform>().sizeDelta = Vector2.MoveTowards(transform.GetChild(0).GetComponent<RectTransform>().sizeDelta, new Vector2(871,0f), Time.deltaTime*(650-transform.GetChild(0).GetComponent<RectTransform>().sizeDelta.y*2f));
 			pause1.rectTransform.anchoredPosition3D = Vector3.MoveTowards(pause1.rectTransform.anchoredPosition3D,new Vector3(-500f,0,0),Mathf.Abs(pause1.rectTransform.anchoredPosition3D.x*10f-1f)*Time.deltaTime);
